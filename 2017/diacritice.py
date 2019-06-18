1426c4ddd387a4f6dbe457ee96638db05ee94043
Razvan <hulearazvan357@gmail.com>
2019-06-18 23:44:46 +0300

    -2017 folder

---
+++ 2017/diacritice.py
@@ -0,0 +1,16 @@
+import unidecode
+import codecs
+
+filepath = 'neradiate4.csv'
+f = open('final2017_4.csv', 'w')
+
+with codecs.open(filepath,'r', 'utf-8') as fp:  
+   line = fp.readline()
+   cnt = 1
+   while line:
+       line = fp.readline()
+       unaccented_string = unidecode.unidecode(line)
+       f.write(unaccented_string)
+       cnt += 1
+
+f.close()
